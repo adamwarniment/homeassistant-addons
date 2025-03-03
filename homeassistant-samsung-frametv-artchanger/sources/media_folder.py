adfb551d5212ed19c0004fc463d27ff9a50b9596
Adam Warniment <adamwarniment@gmail.com>
2025-03-03 11:02:46 -0500

    Adapted for file dir

--- homeassistant-samsung-frametv-artchanger/sources/media_folder.py
+++ homeassistant-samsung-frametv-artchanger/sources/media_folder.py
@@ -4,10 +4,14 @@ import random
 from io import BytesIO
 from typing import List, Tuple, Optional, Dict
 
-folder_path = 'frame'
+folder_path = '/media/frame'
 
 def get_media_folder_images() -> List[str]:
     """Get a list of JPG/PNG files in the folder, and search recursively if you want to use subdirectories"""
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    logging.info(f"Files in {folder_path}:")
+    for file in files:
+        logging.info(f"  - {file}")
     return [os.path.join(root, f) for root, dirs, files in os.walk(folder_path) for f in files if f.endswith('.jpg') or f.endswith('.png')]
 
 def get_image_url(args):
