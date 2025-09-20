f97baa3ee67a260b49875bc205ef9e7b366dcb73
Adam Warniment <adamwarniment@gmail.com>
2025-09-20 12:20:57 -0400

    Updated to handle new HEIC/HEIF from immich

--- homeassistant-samsung-frametv-artchanger/sources/media_folder.py
+++ homeassistant-samsung-frametv-artchanger/sources/media_folder.py
@@ -4,21 +4,20 @@ import random
 from io import BytesIO
 from typing import List, Tuple, Optional, Dict
 from PIL import Image
+from pillow_heif import register_heif_opener
 
-#folder_path = '/media/frame'
+# Register the HEIF opener to enable Pillow to read HEIC/HEIF files
+register_heif_opener()
 
 def get_media_folder_images(folder_path: str) -> List[str]:
-    """Get a list of JPG/PNG/HEIC files in the folder, and search recursively if you want to use subdirectories"""
-    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-    #logging.info(f"Files in {folder_path}:")
-    #for file in files:
-    #    logging.info(f"  - {file}")
-    return [os.path.join(root, f) for root, dirs, files in os.walk(folder_path) for f in files if f.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.heic', '.HEIC'))]
+    """Get a list of supported image files in the folder, and search recursively."""
+    supported_extensions = ('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.heic', '.HEIC', '.heif', '.HEIF')
+    return [os.path.join(root, f) for root, dirs, files in os.walk(folder_path) for f in files if f.endswith(supported_extensions)]
 
 def find_portrait_image_url(media_folder_path, exclusions: List[str] = []) -> Optional[str]:
     """
-    Similar to get_image_url, but selects only portrait images (15% taller than wide).
-    Also accepts an exclusions list of filepaths to skip.
+    Selects a portrait image (15% taller than wide) and returns its URL.
+    It accepts an exclusions list of filepaths to skip.
     """
     if media_folder_path:
         folder_path = media_folder_path
@@ -26,14 +25,13 @@ def find_portrait_image_url(media_folder_path, exclusions: List[str] = []) -> Op
         folder_path = '/media/frame'
 
     files = get_media_folder_images(folder_path)
-    random.shuffle(files) # Shuffle the list in place
+    random.shuffle(files)
     if not files:
         logging.info('No images found in the media folder.')
         return None
 
     for file_path in files:
         file_name = os.path.basename(file_path)
-        # Check if the file_path is in the exclusions list
         if file_name in exclusions:
             logging.info(f"Skipping excluded file: {file_path}")
             continue
@@ -43,14 +41,13 @@ def find_portrait_image_url(media_folder_path, exclusions: List[str] = []) -> Op
                 img_data = BytesIO(f.read())
                 img = Image.open(img_data)
                 width, height = img.size
-                if height > width * 1.15:  # Check for portrait orientation (15% taller)
+                if height > width * 1.15:
                     return os.path.basename(file_path)
-                    #return folder_path + '/' + os.path.basename(file_path)
                 else:
                     print('not portrait')
         except (IOError, OSError) as e:
             logging.warning(f"Error processing image {file_path}: {e}. Skipping.")
-            continue  # Skip to the next image if there's an error
+            continue
 
     logging.info('No suitable portrait images found in the media folder.')
     return None
@@ -66,8 +63,6 @@ def get_image_url(args):
     if not files:
         logging.info('No images found in the media folder.')
         return None
-    #for file in files:
-    #    logging.info(f"Image  - {file}")
     selected_file = random.choice(files)
     return f"{os.path.basename(selected_file)}"
 
@@ -80,10 +75,13 @@ def get_image_direct_path(media_folder_path, image_url) -> Tuple[Optional[BytesI
 
     full_path = os.path.join(folder_path, image_url)
     if not os.path.exists(full_path):
-        logging.error(f"File not found at all: {full_path}")
+        logging.error(f"File not found: {full_path}")
         return None, None
     
-    file_type = 'JPEG' if full_path.endswith(('.jpg','jpeg','.JPEG','.JPG')) else 'PNG' if full_path.endswith(('.png','.PNG')) else 'HEIC' if full_path.endswith(('.heic','.heif', '.HEIC', '.HEIF')) else 'unknown'
+    file_type = 'JPEG' if full_path.endswith(('.jpg', '.jpeg', '.JPEG', '.JPG')) else \
+                'PNG' if full_path.endswith(('.png', '.PNG')) else \
+                'HEIC' if full_path.endswith(('.heic', '.heif', '.HEIC', '.HEIF')) else \
+                'unknown'
     with open(full_path, 'rb') as f:
         data = BytesIO(f.read())
     return data, file_type
@@ -97,10 +95,13 @@ def get_image(args, image_url) -> Tuple[Optional[BytesIO], Optional[str]]:
 
     full_path = os.path.join(folder_path, image_url)
     if not os.path.exists(full_path):
-        logging.error(f"File not found at all: {full_path}")
+        logging.error(f"File not found: {full_path}")
         return None, None
     
-    file_type = 'JPEG' if full_path.endswith(('.jpg','jpeg','.JPEG','.JPG')) else 'PNG' if full_path.endswith(('.png','.PNG')) else 'HEIC' if full_path.endswith(('.heic','.heif', '.HEIC', '.HEIF')) else 'unknown'
+    file_type = 'JPEG' if full_path.endswith(('.jpg', '.jpeg', '.JPEG', '.JPG')) else \
+                'PNG' if full_path.endswith(('.png', '.PNG')) else \
+                'HEIC' if full_path.endswith(('.heic', '.heif', '.HEIC', '.HEIF')) else \
+                'unknown'
     with open(full_path, 'rb') as f:
         data = BytesIO(f.read())
     return data, file_type
